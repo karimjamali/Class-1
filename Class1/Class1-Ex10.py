64a97e143dd0a9bd0e177481a6a858638bfd3766
karimjamali <karim.jamali@gmail.com>
2016-07-22 17:20:16 +0400

    Class1-Ex10

--- Class1/Class1-Ex10.py
+++ Class1/Class1-Ex10.py
@@ -4,28 +4,17 @@ from ciscoconfparse import CiscoConfParse
 from pprint import pprint
 import re
 def main():
-
- cfg=CiscoConfParse('Config-file.txt')
- temp_list=cfg.find_objects_wo_child(parentspec=r"^crypto map", childspec="AES")
- print temp_list
+	cfg=CiscoConfParse('Config-file.txt')
+	temp_list=cfg.find_objects_wo_child(parentspec=r"^crypto map", childspec="AES")
+	print temp_list
  #print temp_list
  
- for entry in temp_list:
-  for child in entry.children:
-   if "transform" in child.text:
-    match=re.search(r"set transform-set (.*)$", child.text)
-    encryption=match.group(1)
-   # print encryption
-    print "Entry not using AES is " , entry.text , " and its encryption is " , encryption
-
-#for entry in temp_list:
-
- #for entry in temp_list:
- #  print "Crypto Map not using AES " , entry.text 
-   
- # temporary=entry.children
- # for item in temporary:
- #  print item.text
+	for entry in temp_list:
+ 		for child in entry.children:
+   			if "transform" in child.text:
+    			match=re.search(r"set transform-set (.*)$", child.text)
+    			encryption=match.group(1)
+  			    print "Entry not using AES is " , entry.text , " and its encryption is " , encryption
   
 
 if __name__ == "__main__":
